401ee9d1208eef04014143849358db256aebb2eb
EC2 Default User <ec2-user@ip-172-31-9-83.ap-northeast-1.compute.internal>
2023-05-28 08:22:18 +0000

    退会画面作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :orders, through: :order_details
+
+  
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
